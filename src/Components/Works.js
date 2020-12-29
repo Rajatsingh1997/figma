be3372a4564cccd02693d3f50751edcf7f632648
Rajatsingh1997 <rajat@excellancetechnologies.info>
2020-12-29 17:35:02 +0530

    first

---
+++ src/Components/Works.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Works() {
+    return (
+        <div>
+            <h1 className="text-center">How it Works</h1>
+            <div className="row">
+                <div className="left">
+                <p className="lefttake">Ideate & Collect</p>
+                </div>
+                <div className="right"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Works
